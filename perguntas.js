0c4ee2fc022d3482f35425accfc050124cfbb340
F12UZA <kaua.baldega@escola.pr.gov.br>
2025-08-13 12:52:28 +0000

    cartoes de perguntas e respostas

--- perguntas.js
+++ perguntas.js
@@ -3,4 +3,22 @@ criaCartao(
     'O que é Javascript?',
     'É uma linguagem de programação.'
 )
-
+criaCartao(
+    "Programação",
+    "oque o agro mais produz?",
+    "Dos principais produtos agropecuários do Brasil, cabe destaque para a cana-de-açúcar, o café e a laranja"
+)
+criaCartao(
+"Programação",
+"O que é a concentração fundiária no Brasil?",
+"A concentração fundiária refere-se à distribuição desigual da terra, onde uma pequena parcela da população possui a maior parte das terras agricultáveis"
+)
+criaCartao(
+    "Programação",
+    "Como a agricultura pode enfrentar a crise hídrica?",
+    "Adoção de práticas de manejo sustentável da água, como a captação de água da chuva e a revitalização de mananciais, são importantes"
+)
+criaCartao(
+    "Programação",
+    "Quais são os setores do agronegócio?",
+)
